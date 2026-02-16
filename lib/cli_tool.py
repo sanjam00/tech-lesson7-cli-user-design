5f6da1314ef97caedbebae77d3f2dc8db3231900
Sanaya James <sanaya.j.james@gmail.com>
2026-02-15 18:42:17 -0700

    Completed lesson. Implemented main() and argaprse

--- lib/cli_tool.py
+++ lib/cli_tool.py
@@ -16,3 +16,32 @@
 # - For "add", require a "description" argument and set its handler
 # - For "list", just set the handler to list_tasks
 # - Parse the arguments and call the appropriate handler (if exists)
+
+import argparse
+
+def add_task(args):
+  print(f"✅ Task added: {args.description}")
+
+def list_tasks(args):
+  print(f"Listing all tasks...")
+
+def main():
+  parser = argparse.ArgumentParser(description = "Task Manager CLI")
+  subparsers = parser.add_subparsers()
+
+  #Add command
+  add_parser = subparsers.add_parser("add", help="Add a new task")
+  add_parser.add_argument("description", help="Description of the task")
+  add_parser.set_defaults(func=add_task)
+
+  #List command
+  add_parser = subparsers.add_parser("list", help="List all tasks")
+  add_parser.set_defaults(func=list_tasks)
+
+  args = parser.parse_args()
+
+  #Handle missing command
+  if hasattr(args, "func"):
+    args.func(args)
+  else:
+    parser.print_help()
--- task_cli.py
+++ task_cli.py
@@ -6,3 +6,8 @@ Entry point for the CLI tool.
 
 # TODO: Create an if __name__ == "__main__" block
 # Inside it, call main()
+
+from lib.cli_tool import main
+
+if __name__ == "__main__":
+  main()
